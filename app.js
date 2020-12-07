0a201e3670da97859d79da7f257e41656a769baa
xw-123 <>
2020-12-07 13:07:45 -0500

    Update app.js
    Add something.

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const configRoutes = require('./routes')
 const exphbs = require('express-handlebars')
+const cookie = require('cookie-parser');
 const session = require('express-session')
 const staticpage = express.static(__dirname + '/public')
 
+app.use(cookie());
+app.use(express.json());
+
 app.use('/public', staticpage)
 
+app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
