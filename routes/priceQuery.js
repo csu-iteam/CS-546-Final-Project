9745ffb3c12b89445bd6fdd95666846df876fb90
dccif <heyujieboy@163.com>
2020-12-08 16:11:31 +0800

    divide render and api request

--- routes/priceQuery.js
+++ routes/priceQuery.js
@@ -3,9 +3,8 @@ const axios = require('axios')
 const router = express.Router()
 const hotelApi = require('../config/hotelApi')
 
-
-async function renderHotelList(data, res) {
-    hotelList = await data.suggestions.find(o => {
+function renderHotelList(data, res) {
+    hotelList = data.suggestions.find(o => {
         return o.group === 'HOTEL_GROUP'
     }).entities
     res.render('layouts/hotel', {hotel: hotelList})
@@ -22,14 +21,11 @@ router.get('/hotel/:loc', async (req, res) => {
         hotelApi.hotel.params = queryParms
         const queryData = await axios.request(hotelApi.hotel).then(async function (response) {
             console.log(response.data)
-            hotelList = await response.data.suggestions.find(o => {
-                return o.group === 'HOTEL_GROUP'
-            }).entities
-            res.render('layouts/hotel', {hotel: hotelList})
-
+            return response.data
         }).catch(function (error) {
             console.log(error)
         })
+        renderHotelList(queryData, res)
     }
 })
 
