db04704b10054b42be19816f1c033ebc979c9a1c
dccif <heyujieboy@163.com>
2020-12-12 22:34:17 +0800

    Format code

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ app.use('/public', staticpage)
 
 // CORS support settings
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
 });
 
 app.use(session({
-	name: 'UserInformation',
-	secret: 'UserSecret',
-	resave: false,
-	saveUninitialized: true
+    name: 'UserInformation',
+    secret: 'UserSecret',
+    resave: false,
+    saveUninitialized: true
 }))
 
 app.use(express.urlencoded({extended: true}))
