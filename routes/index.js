030f6d9acebe4ce450e626057c37667183067662
Doom1Nator <49633152+Doom1Nator@users.noreply.github.com>
2020-12-12 18:02:57 +0800

    Something about recommend page
    Because something wrong with my network, I cannot axios.request data from API, so I committed a local json file for temporarily use. The relative code is commentted in routes/recommendPage.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const rootRoutes = require('.')
 const priceRoutes = require('./priceQuery')
 const login = require('./login');
+const recommendPageRoutes = require('./recommendPage');
 
 const constructorMethod = (app) => {
     app.use('/price', priceRoutes)
     app.use('/login', login);
+    app.use('/recommend', recommendPageRoutes);
 
     app.use('*', (req, res) => {
         res.render('layouts/main')
