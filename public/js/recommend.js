bc3d5f52588197e1945fdf7822300fce0575a0c1
Doom1Nator <49633152+Doom1Nator@users.noreply.github.com>
2020-12-14 03:15:16 +0800

    about more around

--- public/js/recommend.js
+++ public/js/recommend.js
@@ -2,8 +2,9 @@
     var recommendPicsDivOrigin = $('#recommendPics-div-origin');
     var recommendPicsDivMore = $('#recommendPics-div-more');
     var moreButton = $('#more-button');
+    var moreAround = $('.more-around');
 
-    moreButton.click(function (event) {
+    moreButton.on('click', function (event) {
         event.preventDefault();
 
         const tagsLabels = [
@@ -66,15 +67,67 @@
             }
 
             var dataLists = queryLists(picData[0]);
+            var newDiv = [];
             for (let i = 0; i < dataLists.length; i++) {
                 let targetPic = dataLists[i];
 
-                recommendPicsDivMore.append(`<img src=${targetPic.url.source_url} alt=${targetPic.name} width="384px" height="216px" class="image">`);
-                recommendPicsDivMore.append(`<p>${targetPic.name}</p>`);
-                recommendPicsDivMore.append(`<p>${targetPic.location_id}</p>`);
-                recommendPicsDivMore.append(`<p>${targetPic.snippet}</p>`);
+                recommendPicsDivMore.append(`<div class="more-around-div"></div>`);
+
+                if (i == 0) {
+                    newDiv[i] = recommendPicsDivMore.children('div');
+                } else {
+                    newDiv[i] = newDiv[i - 1].next();
+                }
+
+                newDiv[i].append(`<img src=${targetPic.url.source_url} alt=${targetPic.name} width="384px" height="216px" class="image">`);
+                newDiv[i].append(`<p>${targetPic.name}</p>`);
+                newDiv[i].append(`<span>Location: </span><p>${targetPic.location_id}</p>`);
+                newDiv[i].append(`<p>Description: ${targetPic.snippet}</p>`);
+                newDiv[i].append(`<button class="more-around">See more around this place</button>`);
             }
 
+        })
+        moreButton.text("More");
+    })
+
+
+    moreAround.each(function () {
+
+        $(this).on('click', function (event) {
+
+            event.preventDefault();
+
+            const location_id = $(this).prev().prev().text();
+            console.log(location_id)
+            var requestConfig = {
+                method: 'GET',
+                url: `https://www.triposo.com/api/20201111/poi.json?location_id=${location_id}&account=T9TV2POT&token=2wve45tezxoq0kvv3dpd4odygaeb50rq`
+            };
+
+            $.ajax(requestConfig).then(function (responseMessage) {
+
+                var picData = $(responseMessage);
+                if (picData) {
+                    recommendPicsDivOrigin.hide();
+                    recommendPicsDivMore.empty();
+                    recommendPicsDivMore.show();
+                }
+
+                var dataLists = queryLists(picData[0]);
+                if (dataLists) {
+
+                    for (let i = 0; i < dataLists.length; i++) {
+                        let targetPic = dataLists[i];
+
+                        recommendPicsDivMore.append(`<img src=${targetPic.url.source_url} alt=${targetPic.name} width="384px" height="216px" class="image">`);
+                        recommendPicsDivMore.append(`<p>${targetPic.name}</p>`);
+                        recommendPicsDivMore.append(`<span>Location: </span><p>${targetPic.location_id}</p>`);
+                        recommendPicsDivMore.append(`<p>Description: ${targetPic.snippet}</p>`);
+                    }
+                }
+
+            })
+            moreButton.text("Not here");
         })
     })
 
@@ -83,21 +136,24 @@
 
         let dataLists = [];
         const imagePerPage = 6;
+        console.log(data)
+        if (data.results) {
 
-        for (let i = 0; i < imagePerPage; i++) {
+            for (let i = 0; i < imagePerPage; i++) {
 
-            dataLists[i] = {
+                dataLists[i] = {
 
-                name: data.results[i].name,
-                url: data.results[i].images[0],
-                location_id: data.results[i].location_id,
-                snippet: data.results[i].snippet,
-                coordinates: data.results[i].coordinates,
-                score: data.results[i].score,
-                booking_info: data.results[i].booking_info,
-                attribution: data.results[i].attribution,
-                price_tier: data.results[i].price_tier,
+                    name: data.results[i].name,
+                    url: data.results[i].images[0],
+                    location_id: data.results[i].location_id,
+                    snippet: data.results[i].snippet,
+                    coordinates: data.results[i].coordinates,
+                    score: data.results[i].score,
+                    booking_info: data.results[i].booking_info,
+                    attribution: data.results[i].attribution,
+                    price_tier: data.results[i].price_tier,
 
+                }
             }
         }
         return dataLists
