940c1cd7c9f34845bd862facc60d22381045a4f7
Doom1Nator <49633152+Doom1Nator@users.noreply.github.com>
2020-12-18 02:27:44 +0800

    1

--- routes/planGenerator.js
+++ routes/planGenerator.js
@@ -20,9 +20,9 @@ router.post('/generate_plan', async (req, res) => {
         let plan = await planGenerator.findLowestCostPlan(sourceNodeList);
 
         //res.json({ plan: planDataList });
-        // planDataList = plan;
-        res.render('plan/planDetail', { plan: planDataList })
-        //console.log(planDataList);
+        planDataList = plan;
+        // res.render('plan/planDetail', { plan: planDataList })
+        // console.log(planDataList);
     } catch (e) {
         console.log(e);
         res.status(500).json({ error: "service faild" });
@@ -33,6 +33,9 @@ router.get('/show_plan', async (req, res) => {
     try {
         if (dataRecievedByRecommend) {
             planDataList = dataRecievedByRecommend;
+        }
+        if (planDataList) {
+            planDataList = planDataList;
         }
         console.log(planDataList)
         res.render('plan/planDetail', { plan: planDataList });
@@ -51,12 +54,12 @@ router.get('/getPlace/:searchTerm', async (req, res) => {
     }
 })
 
-let dataRecievedByRecommend = [];
-router.post('/addPlaceFromRecommend', async (req, res) => {
-    let theData = req.body.thisPlaceData;
-    res.json(theData);
-    dataRecievedByRecommend.push(theData);
-    // console.log(dataRecievedByRecommend)
-})
+// var dataRecievedByRecommend = [];
+// router.post('/addPlaceFromRecommend', async (req, res) => {
+//     let theData = req.body.thisPlaceData;
+//     res.json(theData);
+//     dataRecievedByRecommend.push(theData);
+//     // console.log(dataRecievedByRecommend)
+// })
 
 module.exports = router;
